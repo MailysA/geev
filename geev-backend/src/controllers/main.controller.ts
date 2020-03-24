dd586efc366faf4f238a55d135f461e3d04a8a24
Maïlys A <ma.airouche@groupeonepoint.com>
2020-03-24 16:18:49 +0100

    feat(mongodb) connection ok

--- geev-backend/src/controllers/main.controller.ts
+++ geev-backend/src/controllers/main.controller.ts
@@ -8,16 +8,13 @@ export class AnnoncesController {
     this.getAnnonces()
   }
 
-  public getAnnonces(){
-      this.app.get('/annonces', (req: Request, res: Response) => { 
-        annonceModel.find({}, (err, annonces) => { 
-          console.log(annonces)
-          annonces.forEach(annonce => {
-            console.log(annonce._id)
-          })
-          return res.send(annonces)
-        }).catch(err => 
-          console.log(err.message));
-    });
+  getAnnonces(){
+    this.app.get('/annonces', (req: Request, res: Response) => { 
+      console.log(res)
+      annonceModel.find({}, (err, annonces) => { 
+        return res.send(annonces)
+      }).catch(err => 
+        console.log(err.message));
+      });
   }
 }
--- geev-backend/src/models/annonce.model.ts
+++ geev-backend/src/models/annonce.model.ts
@@ -6,25 +6,10 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 var connection = mongoose.connection;
 
 const annonceSchema: Schema = new Schema({
-  data:[
-    {_id: { type: String},
+    _id: { type: String},
     title: { type: String},
     pictures: { type: Array}}
-  ]
-});
-
-const Annonce = connection.model('Annonce', annonceSchema)
-
-async function insertData(){
-  const u = new Annonce(jsonData);
-  u.save(function(err, u){
-    if (err) return console.error(err);
-  });
-}
-
-insertData();
-async function test() {console.log(await Annonce.collection.find({ data:["_id"]})) };
-test();
+);
 
 connection.on('error', console.error.bind(console, 'MongoDB connection error:')); 
 
